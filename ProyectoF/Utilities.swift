eb74e9978d2b1e93a23e49178327470bcdec596c
tonyyasi <tonyyasi@gmail.com>
2017-09-01 09:14:52 -0500

    Adds utility file to extract and generalize deletion of children nodes

---
+++ ProyectoF/Utilities.swift
@@ -0,0 +1,20 @@
+//
+//  Utilities.swift
+//  ProyectoF
+//
+//  Created by Tony Toussaint on 9/1/17.
+//  Copyright © 2017 Jose Andres Salazar. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Utilities {
+    
+    func deleteChildren(node: SKNode) {
+        for child in node.children {
+            child.removeFromParent()
+        }
+    }
+    
+}
